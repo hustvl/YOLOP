3175115b54744213d677e15c8b73cd01a5fa27b7
D.Wu <49116311+Riser6@users.noreply.github.com>
2021-10-28 10:35:24 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,8 +4,10 @@ import torch
 from lib.utils.utils import select_device
 from lib.config import cfg
 from lib.models import get_net
+from pathlib import Path
+import os
 
-def yolop(pretrained=True, weights = "https://github.com/hustvl/YOLOP/blob/main/weights/End-to-end.pth", device="cpu"):
+def yolop(pretrained=True, device="cpu"):
     """Creates YOLOP model
     Arguments:
         pretrained (bool): load pretrained weights into the model
@@ -17,7 +19,10 @@ def yolop(pretrained=True, weights = "https://github.com/hustvl/YOLOP/blob/main/
     device = select_device(device = device)
     model = get_net(cfg)
     if pretrained:
-        model.load_state_dict(torch.hub.load_state_dict_from_url(weights, map_location=device, progress=False))
+        path = os.path.join(Path(__file__).resolve().parent, "weights/End-to-end.pth")
+        checkpoint = torch.load(path, map_location= device)
+        model.load_state_dict(checkpoint['state_dict'])
+    model = model.to(device)
     return model
 
 
